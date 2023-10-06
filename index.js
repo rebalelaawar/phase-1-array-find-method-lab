ee51ea4e8fb04809e8bf825924fb63c423d4ec97
rebalelaawar <rebalelaawar@gmail.com>
2023-10-06 11:08:52 -0400

    Array find lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+// function superbowlWin(years){
+//     const winYear = years.find(win =>win.result ==="W");
+//     if (winYear){
+//         return winYear.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+
+
+    
+function superbowlWin(years){
+    const winYear = years.find(win=> win.result==="W");
+    if (winYear){
+        return winYear.year
+    } else {
+        return undefined
+    }
+
+}
+
+
+    
+  
+
